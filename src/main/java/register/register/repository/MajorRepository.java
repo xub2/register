cefc2ebafb1a5076af15f203ca14be806293afb0
xuv2 <fish5478@naver.com>
2025-10-31 14:59:40 +0900

    불필요 레포지토리 삭제

--- src/main/java/register/register/repository/MajorRepository.java
+++
@@ -1,9 +0,0 @@
-package register.register.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import register.register.domain.Major;
-
-@Repository
-public interface MajorRepository extends JpaRepository<Major, Long> {
-}
--- src/main/java/register/register/repository/ProfessorRepository.java
+++
@@ -1,9 +0,0 @@
-package register.register.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import register.register.domain.Professor;
-
-@Repository
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-}
