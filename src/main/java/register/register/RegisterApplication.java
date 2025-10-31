33c04f8c4651d4f86794d43c03922fa35d784774
xuv2 <fish5478@naver.com>
2025-10-31 17:23:33 +0900

    변경

--- src/main/java/register/register/RegisterApplication.java
+++ src/main/java/register/register/RegisterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RegisterApplication {
 
     public static void main(String[] args) {
-        System.out.println("외않되");
+        System.out.println("외않되???????????????");
         SpringApplication.run(RegisterApplication.class, args);
     }
 
