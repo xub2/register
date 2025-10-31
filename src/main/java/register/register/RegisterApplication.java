cb232ce3c5c1ee4afeb24d98d10c05508ba72353
xuv2 <fish5478@naver.com>
2025-10-31 16:45:26 +0900

    이번엔 진짜 돼라

--- src/main/java/register/register/RegisterApplication.java
+++ src/main/java/register/register/RegisterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RegisterApplication {
 
     public static void main(String[] args) {
-        System.out.println("제발 돼라");
+        System.out.println("외않되");
         SpringApplication.run(RegisterApplication.class, args);
     }
 
