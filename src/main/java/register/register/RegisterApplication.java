5352edea4bfc8b21424a9ce888e5d738cdee7868
xuv2 <fish5478@naver.com>
2025-10-31 17:38:32 +0900

    변경

--- src/main/java/register/register/RegisterApplication.java
+++ src/main/java/register/register/RegisterApplication.java
@@ -9,6 +9,8 @@ public class RegisterApplication {
     public static void main(String[] args) {
         System.out.println("외않되???????????????");
         System.out.println("좀 쳐 돼라고");
+        System.out.println("좀 쳐 돼라고");
+        System.out.println("좀 쳐 돼라고");
         SpringApplication.run(RegisterApplication.class, args);
     }
 
