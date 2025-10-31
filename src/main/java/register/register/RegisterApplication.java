68cfe0874fc361efab451de35166dc1311ffc295
xuv2 <fish5478@naver.com>
2025-10-31 15:11:25 +0900

    workflow test commit

--- src/main/java/register/register/RegisterApplication.java
+++ src/main/java/register/register/RegisterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RegisterApplication {
 
     public static void main(String[] args) {
+        System.out.println("커밋 드가자 ~");
         SpringApplication.run(RegisterApplication.class, args);
     }
 
