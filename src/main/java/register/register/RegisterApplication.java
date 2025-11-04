c5ecdecb529dacb9243b6cfb70fd1df36fccd52c
xuv2 <fish5478@naver.com>
2025-11-04 11:59:28 +0900

    CI/CD 환경 테스트 4일차 #2

--- src/main/java/register/register/RegisterApplication.java
+++ src/main/java/register/register/RegisterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RegisterApplication {
 
     public static void main(String[] args) {
-        System.out.println("가보자~");
+        System.out.println("가보자~ 2트째");
         SpringApplication.run(RegisterApplication.class, args);
     }
 
