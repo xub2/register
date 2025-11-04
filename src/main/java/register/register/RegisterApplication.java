aadcaccb67a2ad77c4a576b327bddc4939576fd2
xuv2 <fish5478@naver.com>
2025-11-04 12:27:14 +0900

    CI/CD 환경 테스트 4일차 #6

--- src/main/java/register/register/RegisterApplication.java
+++ src/main/java/register/register/RegisterApplication.java
@@ -9,7 +9,7 @@ public class RegisterApplication {
     // 시작전 github repo secret 에 EC2 엔드포인트 수정
 
     public static void main(String[] args) {
-        System.out.println("가보자~ 2트째");
+        System.out.println("가보자~ 3트째");
         SpringApplication.run(RegisterApplication.class, args);
     }
 
