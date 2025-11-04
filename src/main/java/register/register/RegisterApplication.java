50202972ed4bb39ecd5c5e842f850c3d89df0ad3
xuv2 <fish5478@naver.com>
2025-11-04 12:03:40 +0900

    CI/CD 환경 테스트 4일차 #3

--- src/main/java/register/register/RegisterApplication.java
+++ src/main/java/register/register/RegisterApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RegisterApplication {
 
+    // 시작전 github repo secret 에 EC2 엔드포인트 수정
+
     public static void main(String[] args) {
         System.out.println("가보자~ 2트째");
         SpringApplication.run(RegisterApplication.class, args);
